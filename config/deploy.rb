22ae10f7372fcd53003ddce555e6aba1387b85b3
ireDave <davidoconnellire@gmail.com>
2019-06-08 11:24:35 +0100

    change to reflect the use of puma over passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,37 +1,76 @@
+server '34.245.155.123', port: 22, roles: [:web, :app, :db], primary: true
+
 set :application, "icu_ratings_app"
 
-set :repo_url, "https://github.com/ninkibah/icu_ratings_app.git"
-set :branch, "master"
-
-set :deploy_to, "/var/apps/ratings"
-
+set :repo_url, "git@github.com:ireDave/ratings.git"
+set :branch, "rvm_nginx_puma_rds"
+set :user, "deploy"
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{log tmp/pids public/system public/webalizer}  # capistrano/rails adds public/assets
+set :linked_dirs, %w{log tmp/pids public/system public/sm public/live} 
 
-set :maintenance_file, "public/system/maintenance.html"
 
-set :log_level, :info
+set :pty,             true
+set :use_sudo,        true
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: false, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :rvm_type, :user  
+set :rvm_ruby_version, "2.2.1"
+set :nvm_node, 'v12.4.0'
 
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+namespace :puma do
+    desc 'Create Directories for Puma Pids and Socket'
+    task :make_dirs do
+      on roles(:app) do
+        execute "mkdir #{shared_path}/tmp/sockets -p"
+        execute "mkdir #{shared_path}/tmp/pids -p"
+      end
+    end
+  
+    before :start, :make_dirs
+  end
+  
+  namespace :deploy do
+    desc "Make sure local git is in sync with remote."
+    task :check_revision do
+      on roles(:app) do
+        unless `git rev-parse HEAD` == `git rev-parse origin/master`
+          puts "WARNING: HEAD is not the same as origin/master"
+          puts "Run `git push` to sync changes."
+          exit
+        end
+      end
+    end
+  
+    desc 'Initial Deploy'
+    task :initial do
+      on roles(:app) do
+        before 'deploy:restart', 'puma:start'
+        invoke 'deploy'
+      end
+    end
+  
+    desc 'Restart application'
+    task :restart do
+      on roles(:app), in: :sequence, wait: 5 do
+        invoke 'puma:restart'
+      end
+    end
+  
+    before :starting,     :check_revision
+    after  :finishing,    :compile_assets
+    after  :finishing,    :cleanup
+    after  :finishing,    :restart
+  end
+  
